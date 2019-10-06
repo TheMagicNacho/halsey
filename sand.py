63f84ad3ab8229cd39a7ea8cbbf6ab717146df48
Justin Peter <justinhasanaccount@gmail.com>
2019-10-06 18:01:58 +0200

    improve model database

--- sand.py
+++ sand.py
@@ -1,21 +1,70 @@
-'''
-Create vector CSV for all smiles within a directory
-ONLY USE TO CREATE A DATABASE FOR SUPERVISED TRAINING
-'''
-from PIL import Image
-import face_recognition
+'''Main'''
+import numpy as np
+import pandas as pd
+import os
+
+from sklearn.linear_model import LogisticRegression
+
+'''Data Viz'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+color = sns.color_palette()
+import matplotlib as mpl
+
 import csv
-import glob
+from collections import Counter
 
-csvfile = "primemod.csv"
+'''Data Prep'''
+from sklearn import preprocessing as pp
+from scipy.stats import pearsonr
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.metrics import confusion_matrix, classification_report
+
+'''Algos'''
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import RandomForestClassifier
+# import xgboost as xgb
+# import lightgbm as lgb
 
 
-for file in glob.glob('models/SMILEsmileD-master/SMILEs/positives/positives7/*.jpg'):
-    image = face_recognition.load_image_file(file)
-    face_landmarks_list = face_recognition.face_landmarks(image)
-    pil_image = Image.fromarray(image)
-    for face_landmarks in face_landmarks_list:
-        res = (face_landmarks['bottom_lip'] + face_landmarks['top_lip'])
-        with open(csvfile, "a") as output:
-            writer = csv.writer(output)
-            writer.writerows([res])
+# Acquire Data
+current_path = os.getcwd()
+file = 'models/halseytrainer.csv'
+data = pd.read_csv(file)
+
+# # print(data.columns)
+dataX = data.copy().drop(['class'], axis=1)
+dataY = data['class'].copy()
+
+featuresToScale = dataX.drop(['file'], axis=1).columns
+
+sX = pp.StandardScaler(copy=True)
+dataX.loc[:, featuresToScale] = sX.fit_transform(dataX[featuresToScale])
+scalingFactors = pd.DataFrame(data=[sX.mean_, sX.scale_], index=['Mean', 'StDev'], columns=featuresToScale)
+
+correlationMatrix = pd.DataFrame(data=[], index=dataX.columns, columns=dataX.columns)
+
+
+k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
+
+penalty = 'l2'
+C = 1.0
+class_weight = 'balanced'
+random_state = 2018
+solver = 'liblinear'
+n_jobs = 1
+
+logReg = LogisticRegression(penalty=penalty, C=C, class_weight=class_weight, random_state=random_state, solver=solver, n_jobs=n_jobs)
+
+trainingScores = []
+cvScores = []
+predictionsBasedOnKFolds = pd.DataFrame(data=[], index=y_train.index, columns=[0, 1])
+
+model = logReg
+
+print(model)
